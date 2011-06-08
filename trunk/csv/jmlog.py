5e3b43bde34fe3b9d6ecc416a1866531a31a5569
Pavel Paulau <Pavel.Paulau@gmail.com>
2011-06-08 14:05:30 +0300

    Comments added for CSV log class file

--- trunk/csv/jmlog.py
+++ trunk/csv/jmlog.py
@@ -1,3 +1,7 @@
+####################################################################################################
+#    JMeter CSV Log Class                                                                          #
+####################################################################################################
+
 from numpy import arange
 from matplotlib.dates import MinuteLocator, DateFormatter
 from csv import reader, writer
@@ -7,12 +11,17 @@ rcParams['font.size'] = 8
 
 class jmlog:
     def __init__(self,path):
+        # Open CSV log file from local disk
         log_file = open(path,"r")
+        # Initialize container and read data from log
         self.data=list()
         self.data.extend(reader(log_file))
+        # Close CSV log file
         log_file.close()
-        
+
+        # Appen additional column to data array - Seconds from start
         self.data[0].append("secFromStart")
+        # Obtain indexes for each column
         self.sec_index  =   self.index("secFromStart")
         self.ts_index   =   self.index("timeStamp")
         self.et_index   =   self.index("elapsed")
@@ -22,14 +31,19 @@ class jmlog:
         self.err_index  =   self.index("success")
         self.vu_index   =   self.index("allThreads")
 
+        # Transaction labels
         self.labels = list()
+
+        # Time Variables: fixed and view frame values
         start_time = long(self.data[1][self.ts_index])
         self.start_time = 0
         self.start = 0
         self.end_time = 0
         for row in range(1,len(self.data)):
+            # Calculate additional column value - Seconds from start
             current_time = long(self.data[row][self.ts_index])
             self.data[row].append(int((current_time-start_time)/1000))
+            # Transform string values to integer type
             self.data[row][self.et_index]=int(self.data[row][self.et_index])
             self.data[row][self.lt_index]=int(self.data[row][self.lt_index])
             self.data[row][self.b_index]=int(self.data[row][self.b_index])
@@ -37,31 +51,42 @@ class jmlog:
                 self.data[row][self.vu_index]=int(self.data[row][self.vu_index])
             except:
                 None
+            # Update end time
             if self.end_time < self.data[row][-1]:
                 self.end_time = self.data[row][-1]
+            # Update label list
             if not self.data[row][self.lbl_index] in self.labels:
                 self.labels.append(self.data[row][self.lbl_index])
         self.end = self.end_time
 
     def index(self,column):
+        # Return numerical index for string value (key-value hash)
         for i in range(len(self.data[0])):
             if self.data[0][i] == column:
                 return i
 
     def log_agg(self, time_int, label, mode):
+        # Calculate and average performance metrics (set by 'mode' parameter)
+        # for specified transaction label and time interval 
         prev_step = self.start
         next_step = prev_step+time_int
         points = dict()
         points[prev_step]=0
-        
+
+        # Poor algorithm for start time calculation - to fix!!!
         for i in range(1,len(self.data)):
             if self.data[i][self.sec_index] >= prev_step:
                 row = i
                 break
+
+        # Data points calculation
         count = 0
         while prev_step < self.end and row < len(self.data):
+            # Check whether timestamp in current interval
             if self.data[row][self.sec_index] < next_step:
+                # Is transaction metric?
                 if self.data[row][self.lbl_index] == label:
+                    # Calculate point for each mode (aka metric)
                     if mode == 'bpt':
                         points[prev_step] += self.data[row][self.b_index]
                     elif mode == 'art':
@@ -75,6 +100,7 @@ class jmlog:
                     elif mode == 'err' or mode == 'errc':
                         if self.data[row][self.err_index] == 'false':
                             points[prev_step] += 1
+                # Or aggregative metric?
                 elif mode == 'err_total' or mode == 'errc_total':
                     if self.data[row][self.err_index] == 'false':
                         points[prev_step] += 1
@@ -86,6 +112,7 @@ class jmlog:
                     points[prev_step] = self.data[row][self.vu_index]
                 row += 1
             else:
+                # Finalize averaging
                 if mode == 'errc' or mode == 'errc_total':
                     points[next_step] = points[prev_step]
                 elif mode == 'vusers':
@@ -100,11 +127,13 @@ class jmlog:
                     points[prev_step] /= (time_int*1.0)
                     if next_step < self.end:
                         points[next_step] = 0
+                # Next time interval
                 prev_step = next_step
                 next_step += time_int
         return points
 
     def trend(self,array = list()):
+        # Smooth graph using moving average algorithm
         ma = list()
         for i in range(5):
             ma.append(array[i])
@@ -118,6 +147,7 @@ class jmlog:
         return ma
 
     def outlier_filter(self,array = list()):
+        # Remove outliers using moving median algorithm
         mm = list()
         for i in range(2):
             mm.append(array[2])
@@ -131,46 +161,63 @@ class jmlog:
         return mm
    
     def plot(self, graph = 'bpt_total',time_int = 30, label = None, l_opt = False,ttl=None,trend = False, pnts=False):
+        # Check whether 'Legend' is set and customize plot mode
         if l_opt:
             ax=subplot(2,1,1)
         else:
             ax=subplot(1,1,1)
-
+        
+        # Set graph title    
         title(ttl)
+
+        # Extract data points for specified time interval, transaction label and graph type
         points = self.log_agg(time_int, label, graph)
 
+        # Set graph label
         if graph == 'bpt_total'     : label = 'Total Throughput'
         elif graph == 'rpt_total'   : label = 'Total Hits'
         elif graph == 'err_total'   : label = 'Total Error Rate'
         elif graph == 'errc_total'  : label = 'Total Error Count'
 
+        # Initialize data points arrays
         x=list()
         y=list()
         for key in sorted(points.keys()):
+            # Define time value (X axis)
             days = key/86400
             hours = (key-86400*days)/3600
             minutes = (key - 86400*days-3600*hours)/60
             seconds = key - 86400*days-3600*hours-60*minutes
             days+=1
             x.append(datetime(1970, 1, days, hours, minutes, seconds))
+            # Define time value (Y axis)
             y.append(points[key])
-        if pnts:
+
+        # Check whether 'Points' is set and customize graph
+        if pnts:            
             plot(x,y,linestyle='solid',marker='.',markersize=5,label = label, linewidth=0.5)
-        else:
+        else:            
             plot(x,y,label = label, linewidth=0.5)
+
+        # Check whether 'Trend' is set and customize graph
         if trend:
             plot(x,self.trend(y),label = label+' (Trend)', linewidth=1)
 
+        # Activate grid mode
         grid(True)
+
+        # Evaluate time markers
         max_min = self.end/60
         min_min = self.start/60
         
         time_int = (int((max_min-min_min)/10.0))/10*10
+
         if not time_int:
             if max_min>75:
                 time_int=10
             else:
                 time_int=5
+
         if time_int > 30:
             time_int = 60
 
@@ -184,5 +231,7 @@ class jmlog:
             labels = ax.get_xticklabels()
             ax.xaxis.set_major_formatter( DateFormatter('%d;%H:%M') )
             setp(labels, rotation=0, fontsize=8)
+
+        # Check whether 'Legend' is set and customize graph
         if l_opt:
             legend(bbox_to_anchor=(0, -0.2),loc=2,ncol=1)
