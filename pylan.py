b32871332a229528a942a5c3113e9f82f292239a
Pavel Paulau <pavel.paulau@gmail.com>
2011-09-04 22:25:14 +0300

    Merge license info

--- pylan.py
+++ pylan.py
@@ -1,3 +1,35 @@
+'''
+Copyright (c) 2011, Pavel Paulau <Pavel.Paulau@gmail.com>
+
+All rights reserved.
+
+Redistribution and use of this software in source and binary forms, with or 
+without modification, are permitted provided that the following conditions are 
+met:
+
+* Redistributions of source code must retain the above copyright notice, this 
+list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation 
+and/or other materials provided with the distribution.
+
+* Neither the name of Jan Odvarko nor the names of its contributors may be used 
+to endorse or promote products derived from this software without specific 
+prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR 
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+'''
+
 from pylab import *
 import pygtk, gtk, gobject
 pygtk.require('2.0')
@@ -12,9 +44,9 @@ from lxml import etree
 from datetime import datetime
 import os
 
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
     JMeter Unified Log Class
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 class jmlog:
     def __init__(self,path):
         # Read first log line for further validation        
@@ -427,9 +459,9 @@ class jmlog:
             legend(bbox_to_anchor=(0, -0.2),loc=2,ncol=1)
 
 
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
     Main Class
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 class PyLan:    
     def destroy(self, widget):
         gtk.main_quit()
@@ -838,13 +870,6 @@ class WarnWindow:
             status)
         md.run()
         md.destroy()
-
-class DTDResolver(etree.Resolver):
-    def resolve(self, url, id, context):
-        
-        schema = "<!ELEMENT httpSample EMPTY>"
-        
-        return self.resolve_string(schema, context)
 
 def main():
     PyLan()
