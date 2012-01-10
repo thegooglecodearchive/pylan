6d38bfd7ea2f70e5be3efba95df498940ef6bdce
Pavel Paulau <Pavel.Paulau@gmail.com>
2012-01-10 21:08:21 +0000

    Fixed license text

--- PyLan.py
+++ PyLan.py
@@ -14,7 +14,7 @@ list of conditions and the following disclaimer.
 this list of conditions and the following disclaimer in the documentation 
 and/or other materials provided with the distribution.
 
-* Neither the name of Jan Odvarko nor the names of its contributors may be used 
+* Neither the name of the author nor the names of contributors may be used 
 to endorse or promote products derived from this software without specific 
 prior written permission.
 
